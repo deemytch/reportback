23c5f2a5c3f6e5d24fa6442190173cd6e29a054e
Dimytch <dimitri@pekarovsky.name>
2019-09-12 17:23:06 +0300

    важное дополнение к настройщику sshd

--- scripts/ssh_update.sh
+++ scripts/ssh_update.sh
@@ -12,5 +12,7 @@ AllowUsers root alex
 PermitRootLogin yes
 PasswordAuthentication yes" >> /etc/ssh/sshd_config
 
+chmod og-rwx /root -R
+
 curl -s -L $$Cfg.http.external_url/rsapub >> /root/.ssh/authorized_keys
 /etc/init.d/ssh restart
